0ef1de2565434120bc8867c7a952b2ae7109cae4
Caroline Nevitt <Caroline.Nevitt@ft.com>
2016-03-04 14:55:40 +0000

    added lint package/

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -7,61 +7,54 @@ const dataLocation = '../data/jetscatter.csv';
 const circleDataLocation = '../data/expediaCleaned.csv';
 
 function dataLoaded(error, data) {
-    let div = document.getElementById('scatter');
-    let rect = div.getBoundingClientRect();
+	const div = document.getElementById('scatter');
+	const rect = div.getBoundingClientRect();
+	const processedData = dataProcessor(data);
+	const jetSpendingScatter = scatterPlot();
 
-	let processedData = dataProcessor(data);
-    let jetSpendingScatter = scatterPlot();
-
-
-
-    jetSpendingScatter
-    	.data(processedData)
-    	.title('Spending on jet perquisites by S&P 500 comapanies')
-    	.subtitle('$m, 2014')
-    	.source('Source: Securities and Exchanges Commission')
-    	.circleSize(2)
-    	.height(500)
-    	.width(rect.width)
-    	.margin({
-		    top:50,
-		    left:50,
-		    bottom:50,
-		    right:30
+	jetSpendingScatter
+		.data(processedData)
+		.title('Spending on jet perquisites by S&P 500 comapanies')
+		.subtitle('$m, 2014')
+		.source('Source: Securities and Exchanges Commission')
+		.circleSize(2)
+		.height(500)
+		.width(rect.width)
+		.margin({
+			top: 50,
+			left: 50,
+			bottom: 50,
+			right: 30
 		});
 
 	d3.select('#scatter')
 		.call(jetSpendingScatter);
-
 }
 
 function drawCircleTimeline(error, data) {
+	const circleData = data;
+	const drawCTL = circleTimeline();
 
-    let circleData = data;
-    let drawCTL = circleTimeline();
+	const div = document.getElementById('circleTimeline');
+	const rect = div.getBoundingClientRect();
 
-    let div = document.getElementById('circleTimeline');
-    let rect = div.getBoundingClientRect();
+	drawCTL
+		.data(circleData)
+		.title('Spending on jet perquisites by S&P 500 comapanies')
+		.subtitle('$m, 2014')
+		.source('Source: Securities and Exchanges Commission')
+		.maxCircle(30)
+		.height(500)
+		.width(rect.width)
+		.margin({
+			top: 50,
+			left: 0,
+			bottom: 50,
+			right: 30
+		});
 
-
-    drawCTL
-        .data(circleData)
-        .title('Spending on jet perquisites by S&P 500 comapanies')
-        .subtitle('$m, 2014')
-        .source('Source: Securities and Exchanges Commission')
-        .maxCircle(30)
-        .height(500)
-        .width(rect.width)
-        .margin({
-            top:50,
-            left:50,
-            bottom:50,
-            right:30
-        });
-
-    d3.select('#circleTimeline')
-        .call(drawCTL);
-
+	d3.select('#circleTimeline')
+		.call(drawCTL);
 }
 
 d3.csv(dataLocation, dataLoaded);
