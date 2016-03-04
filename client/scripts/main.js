b24820d9ce03e9da38fb15252e8e827c90c56f11
Robin Kwong <robin.kwong@ft.com>
2016-03-04 15:58:17 +0000

    Merge remote-tracking branch 'origin/caroline-branch-9'

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -7,6 +7,7 @@ const dataLocation = '../data/jetscatter.csv';
 const circleDataLocation = '../data/expediaCleaned.csv';
 
 function dataLoaded(error, data) {
+
     let div = document.getElementById('scatter');
     let rect = div.getBoundingClientRect();
 
@@ -31,36 +32,32 @@ function dataLoaded(error, data) {
 
 	d3.select('#scatter')
 		.call(jetSpendingScatter);
-
 }
 
 function drawCircleTimeline(error, data) {
+	const circleData = data;
+	const drawCTL = circleTimeline();
 
-    let circleData = data;
-    let drawCTL = circleTimeline();
+	const div = document.getElementById('circleTimeline');
+	const rect = div.getBoundingClientRect();
 
-    let div = document.getElementById('circleTimeline');
-    let rect = div.getBoundingClientRect();
+	drawCTL
+		.data(circleData)
+		.title('Spending on jet perquisites by S&P 500 comapanies')
+		.subtitle('$m, 2014')
+		.source('Source: Securities and Exchanges Commission')
+		.maxCircle(30)
+		.height(500)
+		.width(rect.width)
+		.margin({
+			top: 50,
+			left: 0,
+			bottom: 50,
+			right: 30
+		});
 
-
-    drawCTL
-        .data(circleData)
-        .title('Spending on jet perquisites by S&P 500 comapanies')
-        .subtitle('$m, 2014')
-        .source('Source: Securities and Exchanges Commission')
-        .maxCircle(30)
-        .height(500)
-        .width(rect.width)
-        .margin({
-            top:50,
-            left:50,
-            bottom:50,
-            right:30
-        });
-
-    d3.select('#circleTimeline')
-        .call(drawCTL);
-
+	d3.select('#circleTimeline')
+		.call(drawCTL);
 }
 
 d3.csv(dataLocation, dataLoaded);
--- client/scripts/top.js
+++ client/scripts/top.js
@@ -2,32 +2,32 @@
 
 // global addScript function
 function addScript(src, async, defer) {
-  if (!async && !defer) {
-    document.write('<script src="' + src + '">\x3c/script>');
-  }
-  else {
-    var script = document.createElement('script');
-    script.src = src;
-    script.async = !!async;
-    if (defer) script.defer = !!defer;
-    var oldScript = document.getElementsByTagName('script')[0];
-    oldScript.parentNode.appendChild(script);
-    return script;
-  }
+	if (!async && !defer) {
+		document.write('<script src="' + src + '">\x3c/script>');
+	}
+	else {
+		var script = document.createElement('script');
+		script.src = src;
+		script.async = !!async;
+		if (defer) script.defer = !!defer;
+		var oldScript = document.getElementsByTagName('script')[0];
+		oldScript.parentNode.appendChild(script);
+		return script;
+	}
 }
 
 // product-specific cuts-the-mustard test (customise for your needs)
 var cutsTheMustard = (
-  'querySelector' in document &&
-  'localStorage' in window &&
-  'addEventListener' in window
+	'querySelector' in document &&
+	'localStorage' in window &&
+	'addEventListener' in window
 );
 
 // set the root element to .core or .enhanced as appropriate
 if (cutsTheMustard) {
-  document.documentElement.className = (
-    document.documentElement.className.replace(/\bcore\b/g, 'enhanced')
-  );
+	document.documentElement.className = (
+		document.documentElement.className.replace(/\bcore\b/g, 'enhanced')
+	);
 }
 
 // add a polyfill.io script.
