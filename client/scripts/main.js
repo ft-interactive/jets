3bd8a328d4f237f14942972b4233f27d5f79fc63
Robin Kwong <robin.kwong@ft.com>
2016-03-06 19:15:47 +0000

    fixes share icon link and temporary fix for https

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,10 +1,8 @@
 import d3 from 'd3';
 import scatterPlot from './scatter-plot';
 import dataProcessor from './scatter-data-processor';
-import circleTimeline from './circle-timeline';
 
 const dataLocation = '../data/jetscatter.csv';
-const circleDataLocation = '../data/expediaCleaned.csv';
 
 function dataLoaded(error, data) {
 
@@ -35,31 +33,5 @@ function dataLoaded(error, data) {
 		.call(jetSpendingScatter);
 }
 
-function drawCircleTimeline(error, data) {
-	const circleData = data;
-	const drawCTL = circleTimeline();
-	const div = document.getElementById('circleTimeline');
-	const rect = div.getBoundingClientRect();
-
-
-	drawCTL
-		.data(circleData)
-		.title('Spending on jet perquisites by S&P 500 comapanies')
-		.subtitle('$m, 2014')
-		.source('Source: Securities and Exchanges Commission')
-		.maxCircle(30)
-		.height(500)
-		.width(rect.width)
-		.margin({
-			top: 50,
-			left: 0,
-			bottom: 50,
-			right: 30
-		});
-
-	d3.select('#circleTimeline')
-		.call(drawCTL);
-}
-
 d3.csv(dataLocation, dataLoaded);
-d3.csv(circleDataLocation, drawCircleTimeline);
+
