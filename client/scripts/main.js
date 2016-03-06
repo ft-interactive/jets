1241a733a740d4ee41c1133dc77394c8904dc403
Robin Kwong <robin.kwong@ft.com>
2016-03-06 20:24:49 +0000

    https error fixes

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,7 +2,7 @@ import d3 from 'd3';
 import scatterPlot from './scatter-plot';
 import dataProcessor from './scatter-data-processor';
 
-const dataLocation = '../data/jetscatter.csv';
+const dataLocation = 'http://ig.ft.com/sites/business-jets/data/jetscatter.csv';
 
 function dataLoaded(error, data) {
 
